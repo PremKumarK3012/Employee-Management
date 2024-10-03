13f718ca663863c8ec41b7114961364b6d0e7081
PremKumarK3012 <premkpk2004@gmail.com>
2024-10-04 00:14:21 +0530

    delete icon changed

--- src/pages/Employee/Employee.jsx
+++ src/pages/Employee/Employee.jsx
@@ -212,7 +212,7 @@ const Employee = () => {
             <div className="modal-header">
               <div className="col text-center">
                 {" "}
-                <h5 className="modal-title" id="deleteModalLabel">
+                <h5 className="modal-title mb-4" id="deleteModalLabel">
                   Confirm Deletion
                 </h5>{" "}
                 <svg
@@ -226,15 +226,6 @@ const Employee = () => {
                   <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5" />
                 </svg>
               </div>
-              <div></div>
-              <button
-                type="button"
-                className="close"
-                data-dismiss="modal"
-                aria-label="Close"
-              >
-                <span aria-hidden="true">&times;</span>
-              </button>
             </div>
             <div className="modal-body text-center">
               Are you sure you want to delete this employee?
@@ -243,6 +234,13 @@ const Employee = () => {
               <button
                 type="button"
                 className="btn btn-secondary"
+                style={{
+                  padding: "15px",
+                  borderRadius: "5px",
+                  border: "none",
+                  margin: "5px",
+                  backgroundColor: "skyblue",
+                }}
                 data-dismiss="modal"
               >
                 Cancel
@@ -250,7 +248,12 @@ const Employee = () => {
               <button
                 type="button"
                 className="btn btn-danger"
-                onClick={handleDelete}
+                style={{
+                  padding: "15px",
+                  borderRadius: "5px",
+                  border: "none",
+                  backgroundColor: "#df3545",
+                }}
                 data-dismiss="modal"
               >
                 Confirm
